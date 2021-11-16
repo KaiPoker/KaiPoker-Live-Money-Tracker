8333140f1ebf046da2a56155c17fa9c50f84b321
Edward Wang <ewang43@jhu.edu>
2021-11-15 20:05:18 -0500

    Update Node.h with some standard functions

--- CLI/Helper/Node/Node.h
+++ CLI/Helper/Node/Node.h
@@ -1,5 +1,26 @@
 #ifndef __NODE_H__
 #define __NODE_H__
+template<typename T>
+class Node
+{
+    private: 
+        T data;
+        Node *ptrNext;
+        Node *ptrPrev;
+    public:
+        // Constructors
+        Node() : ptrNext(nullptr), ptrPrev(nullptr){}
+        Node(T data) : data(data), ptrNext(nullptr), ptrPrev(nullptr){}
+        
+        // Getters
+        T getData() const {return data;}
+        Node *getNext() const {return ptrNext;}
+        Node *getPrev() const {return ptrPrev;}
 
-#include "Node.inc"
+        // Setters
+        void setData(T data){this -> data = data;}
+        void setNext(Node *ptrNext){this -> ptrNext = ptrNext;}
+        void setPrev(Node *ptrPrev){this -> ptrPrev = ptrPrev;}
+};
+
 #endif
