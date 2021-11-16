e2aa982852d1817f374adf4242061b32581a5a6c
Edward Wang <ewang43@jhu.edu>
2021-11-15 20:13:29 -0500

    Add comments and a few functions to DataLoop.h

--- CLI/Helper/DataLoop/DataLoop.h
+++ CLI/Helper/DataLoop/DataLoop.h
@@ -1,5 +1,18 @@
 #ifndef __DATALOOP_H__
 #define __DATALOOP_H__
 
+#include "../Node/Node.h"
+#include <iostream>
+#include <string>
+
+template<typename T>
+class DataLoop {
+    private:
+        Node *ptrStart; // Pointer to the starting node in the DataLoop
+        size_t count; // Unsigned integer representing the total number of nodes in the DataLoop
+    public:
+        std::string toString() const; // Helper function to convert DataLoop into a std::string
+}
+
 #include "DataLoop.inc"
 #endif
